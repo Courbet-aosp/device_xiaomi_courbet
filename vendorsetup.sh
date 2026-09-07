6654cde5427bf5426d86d0138d5fb77ff4a16a93
Aciss? <wawa22arpk@gmail.com>
2026-09-07 13:25:24 +0700

    Update git clone URLs for various repositories

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,19 +1,19 @@
 # common
-git clone https://github.com/Aciss21/device_xiaomi_sm6150-common.git -b 17 device/xiaomi/sm6150-common
-git clone https://github.com/Aciss21/vendor_xiaomi_sm6150-common.git -b 17 vendor/xiaomi/sm6150-common
+git clone https://github.com/Courbet-aosp/device_xiaomi_sm6150-common.git -b 17 device/xiaomi/sm6150-common
+git clone https://github.com/Courbet-aosp/vendor_xiaomi_sm6150-common.git -b 17 vendor/xiaomi/sm6150-common
 
 # device
-git clone https://github.com/Aciss21/vendor_xiaomi_courbet.git -b 16.2 vendor/xiaomi/courbet
+git clone https://github.com/Courbet-aosp/vendor_xiaomi_courbet.git -b 16.2 vendor/xiaomi/courbet
 
 # kernel
-git clone https://github.com/Aciss21/kernel_xiaomi_sm6150.git -b 16 kernel/xiaomi/sm6150 --depth 1
+git clone https://github.com/Aciss21/kernel_xiaomi_sm6150.git -b 17 kernel/xiaomi/sm6150 --depth 1
 
 # hardware 
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
 
 # Miui Camera
-git clone https://github.com/Aciss21/vendor_miuicamera-courbet.git -b 16 vendor/miuicamera-courbet
+git clone https://github.com/Courbet-aosp/vendor_miuicamera-courbet.git -b 16 vendor/miuicamera-courbet
 
 # DolbyAtmos
-git clone https://github.com/manipvlator/proprietary_vendor_sony_dolby.git vendor/sony/dolby
-git clone https://github.com/manipvlator/android_packages_apps_LunarisDolby.git packages/apps/LunarisDolby
+git clone https://github.com/Courbet-aosp/proprietary_vendor_sony_dolby.git vendor/sony/dolby
+git clone https://github.com/Courbet-aosp/android_packages_apps_LunarisDolby.git packages/apps/LunarisDolby
